0b9623f98543469f158721e3fd7a5ff213cbaeeb
Muhammadattiya <mohammed.a.attiya@gmail.com>
2026-07-24 23:57:01 +0300

    Updates to biggest featureeee

--- backend/run-curl.js
+++ backend/run-curl.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = require('./app'); 
 const User = require('./models/User');
 const Account = require('./models/Account');
+const Category = require('./models/Category');
 const Transaction = require('./models/Transaction');
 
 const PORT = 5555;
@@ -21,8 +22,11 @@ async function runCurl() {
     let user = new User({ name: 'Curl User', email: 'curl@test.com', password: 'password', googleId: 'curl-id', smsWebhookToken: TEST_TOKEN });
     await user.save();
 
-    let account = new Account({ user: user._id, name: 'Curl Account', type: 'bank', balance_adjustment: 0, cardLast4: '2513', currency: 'EGP' });
+    let account = new Account({ user: user._id, name: 'Curl Account', type: 'bank', balance_adjustment: 0, cardLast4: '2513', currency: 'EGP', isDefault: true });
     await account.save();
+
+    let category = new Category({ user: user._id, name: 'Miscellaneous', type: 'expense' });
+    await category.save();
 
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}... Please run curl command manually in another terminal.`);
