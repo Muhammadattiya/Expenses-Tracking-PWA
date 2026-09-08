5e5dc2c5584af232c6b7b33803ecfffda5c095d4
Muhammadattiya <mohammed.a.attiya@gmail.com>
2026-09-08 10:11:59 +0300

    pull to refresh + logo fix

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,6 +4,7 @@ import InstallPrompt from './InstallPrompt';
 import CategoryClarificationManager from './modals/CategoryClarificationManager';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useNetwork } from '../hooks/useNetwork';
+import PullToRefresh from './PullToRefresh';
 
 export default function Layout() {
   const { lang, t } = useLanguage();
@@ -28,7 +29,9 @@ export default function Layout() {
         isAddPage ? "h-full w-full p-0 overflow-hidden max-w-[420px] mx-auto flex flex-col" : 
         "pb-32 px-5 pt-8 w-full max-w-7xl mx-auto min-h-screen animate-fade-in"
       }>
-        <Outlet />
+        <PullToRefresh>
+          <Outlet />
+        </PullToRefresh>
       </main>
       <InstallPrompt />
       <CategoryClarificationManager />
